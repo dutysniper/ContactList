e7b2cd562012515329d6f868d411d6b8c0870c3c
dutysniper <natarov.constantin@yandex.ru>
2023-04-17 23:06:09 +0300

    Setup UI + start work with model

---
+++ Services/DataStore.swift
@@ -0,0 +1,50 @@
+//
+//  DataStore.swift
+//  ContactList
+//
+//  Created by Константин Натаров on 17.04.2023.
+//
+
+let names = [
+"John",
+"Michael",
+"Adam",
+"James",
+"Eric",
+"Isaac",
+"Helen",
+"Andrew"
+]
+
+let surnames = [
+"Black",
+"White",
+"Brown",
+"Gray",
+"Corden",
+"Jefferson",
+"Bols",
+"Lews"
+]
+
+let numbers = [
+"+1 3241 4123 23",
+"+1 3412 6546 54",
+"+1 7685 6534 90",
+"+1 5435 7657 85",
+"+1 1423 7098 54",
+"+1 8756 4367 94",
+"+1 4398 3267 78",
+"+1 8932 7547 95"
+]
+
+let emails = [
+"aaaa@gmail.com",
+"bbbb@gmail.com",
+"cccc@gmail.com",
+"dddd@gmail.com",
+"eeee@gmail.com",
+"ffff@gmail.com",
+"gggg@gmail.com",
+"iiii@gmail.com"
+]
