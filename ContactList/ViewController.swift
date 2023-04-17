76a7b50c0083a5efbbfaeb64944f24939fb3c820
dutysniper <natarov.constantin@yandex.ru>
2023-04-17 22:18:54 +0300

    Initial commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Константин Натаров on 17.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
